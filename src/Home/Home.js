72f5943e73fc8f1d5f5959a8bdc79a7d27732ab0
AhasanUllah <bulbulahasan@gmail.com>
2022-12-11 13:12:24 +0600

    home section data added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Data from './Data';
+import './Home.css';
+import Social from './Social';
 
 const Home = () => {
    return (
       <section className="home section" id="home">
          <div className="home_container container grid">
-            <div className="home_content grid"></div>
+            <div className="home_content grid">
+               <Social></Social>
+
+               <div className="home_img"></div>
+               <Data></Data>
+            </div>
          </div>
       </section>
    );
---
+++ src/Home/Social.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Social = () => {
+   return (
+      <div className="home_social">
+         <a href="https://www.linkedin.com/in/ahasanul/" className="home_social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-linkedin-alt"></i>
+         </a>
+
+         <a href="https://github.com/ahasanUllah" className="home_social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-github-alt"></i>
+         </a>
+
+         <a href="https://www.facebook.com/ahasan07/" className="home_social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-facebook-f"></i>
+         </a>
+      </div>
+   );
+};
+
+export default Social;
