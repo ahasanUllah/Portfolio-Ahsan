e0c47edbc3a4f951dddec26f06383a3bd7a1f5a3
AhasanUllah <bulbulahasan@gmail.com>
2022-12-11 11:28:05 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Header from './Header/Header';
+import Home from './Home/Home';
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className="App">
+         <Header></Header>
+         <main className="main">
+            <Home></Home>
+         </main>
+      </div>
+   );
 }
 
 export default App;
